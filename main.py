9e11000224c150bbe6f998d9f0b223dfe8161f33
ITemik-rr <nikolaev2k11@yandex.ru>
2025-07-20 15:16:29 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное"
+            }
+word = input("Введите непонятное слово (большими буквами!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    seek = input('Введи значение слова')
+    meme_dict[word] = seek
+    print(meme_dict)
